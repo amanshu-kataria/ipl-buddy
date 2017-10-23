87fc65b3bf39f8d15b699a51202014f7b7468e6d
amanshu-kataria <amanshukataria@hotmail.com>
2017-10-23 17:09:29 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import Drawer from "material-ui/Drawer";
