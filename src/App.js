46118d56140301299181aebf864c5f806e08f64a
amanshu-kataria <amanshukataria@hotmail.com>
2017-10-25 01:05:29 +0530

    Removed Drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
-import Drawer from "material-ui/Drawer";
 import MenuItem from "material-ui/MenuItem";
 import IconButton from "material-ui/IconButton";
 import NavigationArrowBack from "material-ui/svg-icons/navigation/arrow-back";
@@ -9,43 +8,15 @@ import Divider from "material-ui/Divider";
 import Stats from "./stats.js";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isDrawerOpen: false
-    };
-    this.handleDrawer = this.handleDrawer.bind(this);
-  }
-
-  handleDrawer() {
-    this.setState({ isDrawerOpen: !this.state.isDrawerOpen });
-  }
   render() {
     return (
       <MuiThemeProvider>
-        <Drawer open={this.state.isDrawerOpen}>
-          <AppBar
-            style={{ backgroundColor: "#424242" }}
-            showMenuIconButton={false}
-            iconElementRight={
-              <IconButton>
-                <NavigationArrowBack />
-              </IconButton>
-            }
-            onRightIconButtonTouchTap={this.handleDrawer}
-            title="Data"
-          />
-          <MenuItem>Matches</MenuItem>
-          <Divider />
-          <MenuItem>Deliveries</MenuItem>
-          <Divider />
-        </Drawer>
         <AppBar
           style={{
             backgroundColor: "#424242",
             position: "fixed"
           }}
-          onLeftIconButtonTouchTap={this.handleDrawer}
+          showMenuIconButton={false}
           title="IPL Buddy"
         />
         <Stats />
