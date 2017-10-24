0d796564a8c35397216fd82b02f31343da1783d7
amanshu-kataria <amanshukataria@hotmail.com>
2017-10-25 01:09:37 +0530

    Removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
-import MenuItem from "material-ui/MenuItem";
-import IconButton from "material-ui/IconButton";
-import NavigationArrowBack from "material-ui/svg-icons/navigation/arrow-back";
-import Divider from "material-ui/Divider";
 import Stats from "./stats.js";
 
 class App extends Component {
